80203c2396a23dc53a3bfffb775ed63634392f6f
Navya <vohrana@sheridancollege.ca>
2025-03-02 18:36:08 -0500

    Added Contents for Home View

--- XCTestApp/View/HomeView.swift
+++ XCTestApp/View/HomeView.swift
@@ -5,3 +5,14 @@
 //  Created by Navya Vohra on 2025-03-02.
 //
 
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        List {
+            NavigationLink("Item 1", destination: DetailView(item: "Item 1"))
+            NavigationLink("Item 2", destination: DetailView(item: "Item 2"))
+        }
+        .navigationTitle("Home")
+    }
+}
