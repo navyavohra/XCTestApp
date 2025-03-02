0049df2d617c073a6b79a699aa65fe461b585db2
Navya <vohrana@sheridancollege.ca>
2025-03-02 18:35:54 -0500

    Added Details view

--- XCTestApp/View/DetailView.swift
+++ XCTestApp/View/DetailView.swift
@@ -5,3 +5,12 @@
 //  Created by Navya Vohra on 2025-03-02.
 //
 
+import SwiftUI
+
+struct DetailView: View {
+    let item: String
+    var body: some View {
+        Text("Details of \(item)")
+            .navigationTitle("Details")
+    }
+}
