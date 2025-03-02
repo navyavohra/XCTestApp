b9b40f787b01124c9bcefb5e65c0035abcb94910
Navya <vohrana@sheridancollege.ca>
2025-03-02 18:33:54 -0500

    Added Authenticaton view model

--- XCTestApp/ViewModel/AuthViewModel.swift
+++ XCTestApp/ViewModel/AuthViewModel.swift
@@ -5,3 +5,25 @@
 //  Created by Navya Vohra on 2025-03-02.
 //
 
+import Foundation
+
+class AuthViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var isAuthenticated: Bool = false
+    @Published var errorMessage: String?
+
+    func login() {
+        guard !username.isEmpty, !password.isEmpty else {
+            errorMessage = "Username and Password cannot be empty"
+            return
+        }
+        
+        // Simulated API Authentication
+        if username == "TestUser" && password == "Password123" {
+            isAuthenticated = true
+        } else {
+            errorMessage = "Invalid credentials"
+        }
+    }
+}
