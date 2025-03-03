88fb74dec48b97754bd50a6948a0309103f0e561
Navya <vohrana@sheridancollege.ca>
2025-03-02 21:24:39 -0500

    Updated XCTestApp

--- XCTestAppUITests/XCTestAppUITests.swift
+++ XCTestAppUITests/XCTestAppUITests.swift
@@ -4,40 +4,42 @@
 //
 //  Created by Navya Vohra on 2025-03-02.
 //
-
 import XCTest
 
-final class XCTestAppUITests: XCTestCase {
+class XCTestAppUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testLoginFlow() {
+        let usernameField = app.textFields["Username"]
+        let passwordField = app.secureTextFields["Password"]
+        let loginButton = app.buttons["Login"]
+        
+        XCTAssertTrue(usernameField.exists)
+        XCTAssertTrue(passwordField.exists)
+        XCTAssertTrue(loginButton.exists)
+        
+        usernameField.tap()
+        usernameField.typeText("TestUser")
+        
+        passwordField.tap()
+        passwordField.typeText("Password123")
+        
+        loginButton.tap()
+        
+        XCTAssertTrue(app.navigationBars["Home"].exists)
+    }
+    
+    func testNavigationToDetailScreen() {
+        let firstItem = app.staticTexts["Item 1"]
+        XCTAssertTrue(firstItem.exists)
+        firstItem.tap()
+        
+        XCTAssertTrue(app.staticTexts["Details of Item 1"].exists)
     }
 }
